b1511e84a3b13ddf3bb7b9c409a05884086a117f
yuslove1 <adexyuslove@gmail.com>
2025-02-24 22:49:08 -0800

    fix: minor change

--- prodev-mobile-app-0x06/constants/index.ts
+++ prodev-mobile-app-0x06/constants/index.ts
@@ -1,5 +1,5 @@
 const BACKGROUNDIMAGE = require('@/assets/images/hero-icon.png')
-const HEROLOGO = require('@/assets/images/Logo.png')
+const HEROLOGO = require('@/assets/images/logo.png')
 const HEROLOGOGREEN = require('@/assets/images/logo-green.png')
 const GOOGLELOGO = require('@/assets/images/google.png')
 const FACEBOOKLOGO = require('@/assets/images/facebook.png')
